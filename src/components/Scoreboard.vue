e0e9a0779fefc23e61a3a5d9e06ba35a21c9eeb8
Bald Mike <baldmike312@gmail.com>
2019-12-22 13:37:03 -0600

    fully functional

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="inningBox"><h1>Current Inning: {{ inning }}</h1></div>
+    <div class="inningBox"><h1>Current Inning: {{ currentInning }}</h1></div>
     <div class="scoreboard">
       
       <table class="table">
@@ -9,28 +9,28 @@
           <th v-for="item in 9" :key="item">{{ item  }}</th>
         </thead>
         <tbody>
-          <td>Home</td>
-          <td v-bind:class="{ active: this.inning === 1 }">{{ homeOne }}</td>
-          <td v-bind:class="{ active: this.inning === 3 }">{{ homeTwo }}</td>
-          <td v-bind:class="{ active: this.inning === 5 }">{{ homeThree }}</td>
-          <td v-bind:class="{ active: this.inning === 7 }">{{ homeFour }}</td>
-          <td v-bind:class="{ active: this.inning === 9 }">{{ homeFive }}</td>
-          <td v-bind:class="{ active: this.inning === 11 }">{{ homeSix }}</td>
-          <td v-bind:class="{ active: this.inning === 13 }">{{ homeSeven }}</td>
-          <td v-bind:class="{ active: this.inning === 15 }">{{ homeEight }}</td>
-          <td v-bind:class="{ active: this.inning === 17 }">{{ homeNine }}</td>
+          <td>Away</td>
+          <td v-bind:class="{ active: this.inning === 0 }">{{ score[0] }}</td>
+          <td v-bind:class="{ active: this.inning === 2 }">{{ score[2]}}</td>
+          <td v-bind:class="{ active: this.inning === 4 }">{{ score[4] }}</td>
+          <td v-bind:class="{ active: this.inning === 6 }">{{ score[6] }}</td>
+          <td v-bind:class="{ active: this.inning === 8 }">{{ score[8] }}</td>
+          <td v-bind:class="{ active: this.inning === 10 }">{{ score[10] }}</td>
+          <td v-bind:class="{ active: this.inning === 12 }">{{ score[12] }}</td>
+          <td v-bind:class="{ active: this.inning === 14 }">{{ score[14] }}</td>
+          <td v-bind:class="{ active: this.inning === 16 }">{{ score[16] }}</td>
         </tbody>
         <tbody>
-          <td>Away</td>
-          <td v-bind:class="{ active: this.inning === 2 }">{{ awayOne }}</td>
-          <td v-bind:class="{ active: this.inning === 4 }">{{ awayTwo }}</td>
-          <td v-bind:class="{ active: this.inning === 6 }">{{ awayThree }}</td>
-          <td v-bind:class="{ active: this.inning === 8 }">{{ awayFour }}</td>
-          <td v-bind:class="{ active: this.inning === 10 }">{{ awayFive }}</td>
-          <td v-bind:class="{ active: this.inning === 12 }">{{ awaySix }}</td>
-          <td v-bind:class="{ active: this.inning === 14 }">{{ awaySeven }}</td>
-          <td v-bind:class="{ active: this.inning === 16 }">{{ awayEight }}</td>
-          <td v-bind:class="{ active: this.inning === 18 }">{{ awayNine }}</td>
+          <td>Home</td>
+          <td v-bind:class="{ active: this.inning === 1 }">{{ score[1] }}</td>
+          <td v-bind:class="{ active: this.inning === 3 }">{{ score[3] }}</td>
+          <td v-bind:class="{ active: this.inning === 5 }">{{ score[5] }}</td>
+          <td v-bind:class="{ active: this.inning === 7 }">{{ score[7] }}</td>
+          <td v-bind:class="{ active: this.inning === 9 }">{{ score[9] }}</td>
+          <td v-bind:class="{ active: this.inning === 11 }">{{ score[11] }}</td>
+          <td v-bind:class="{ active: this.inning === 13 }">{{ score[13] }}</td>
+          <td v-bind:class="{ active: this.inning === 15 }">{{ score[15] }}</td>
+          <td v-bind:class="{ active: this.inning === 17 }">{{ score[17] }}</td>
         </tbody>
       </table>
     </div>
@@ -40,7 +40,7 @@
     </div>
     
     <div class="action-box">
-      <button @click.prevent="swing" class="button">Swing!</button>
+      <button @click.prevent="swing" class="button">PITCH</button>
     </div>
 
     <div class="count-box">
@@ -48,104 +48,259 @@
       <div id = "countBox"> {{ balls }} | {{ strikes }} | {{ outs }}</div>
     </div>
 
-
-
-
+    <div class="field-box">
+        <ul>
+            <li v-if="firstBase">Runner on First</li>
+            <li v-if="secondBase">Runner on Second</li>
+            <li v-if="thirdBase">Runner on Third</li>
+        </ul>
+    </div>
   </div>
-
-   
 </template>
 
 <script>
 export default {
-  name: 'scoreboard',
-  data() {
-  	return {
-      currentInning: 1,
-      balls: 0,
-      strikes: 0,
-      outs: 0,
-      inning: 1,
-      homeOne: 0, homeTwo: 0, homeThree: 0, homeFour: 0, homeFive: 0, homeSix: 0, homeSeven: 0, homeEight: 0, homeNine: 0,
-      awayOne: 0, awayTwo: 0, awayThree: 0, awayFour: 0, awayFive: 0, awaySix: 0, awaySeven: 0, awayEight: 0, awayNine: 0,
-      outcome: [],
-      firstBase: false,
-      secondBase: false,
-      thirdBase: false
-    }},
-    methods: {
-      swing() {
-        
-        // pick a number 1 to 100
-        let number = (Math.floor(Math.random()*100)+1)
-        
-        //strike swinging
-        if (number <=20) {
-          
-            //   let audio = new Audio('http://soundbible.com/grab.php?id=504&type=mp3');
-            //   audio.play();
+    name: 'scoreboard',
+    
+    data() {
+        return {
+            balls: 0,
+            strikes: 0,
+            outs: 0,
+            inning: 0,
+            score: [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+            inningScore: 0,
+            outcome: [],
+            firstBase: false,
+            secondBase: false,
+            thirdBase: false
+        }},
 
-            this.outcome.push("Strike " + this.strikes + "!");
+    computed: {
+        currentInning() {
+            return Math.floor(this.inning/2 + 1);
+        }
+    },
+    
+    methods: {
+
+        swing() {
+            // pick a number 1 to 100
+            let number = (Math.floor(Math.random()*100)+1)
+            
+            //strike swinging
+            if (number <=40) {
+
+                this.strike();
+
+            } else if (number > 40 && number <= 50) {
+                
+                this.ball();
+
+            } else if (number > 50 && number <= 60) {
+                
+                this.groundOut();
+
+            } else if (number > 60 && number <= 72) {
+                
+                this.flyOut();
+
+            } else if (number > 72 && number <= 82) {
+                
+                this.foulBall();
+                
+            } else if (number > 82 && number <= 89) {
+            
+                this.single();
+            
+            } else if (number > 90 && number <= 94) {
+                
+                this.double();
+
+            } else if (number > 94 && number <= 98) {
+                
+                this.triple();
+            
+            } else if (number > 98 && number <= 100) {
+
+                this.homeRun();
+                
+            }
+        },
+
+        strike() {
             this.strikes += 1;
+            this.outcome.push("Strike " + this.strikes + "!");
             
             if(this.strikes === 3) {
+                this.strikes = 0;
                 this.outs +=1;
                 if (this.outs === 3) {
                     this.nextInning();
+                } else {
+                    this.nextBatter();
                 }
             }
-        } else if (number > 20 && number <= 25) {
+        },
+
+        ball() {
+            this.balls += 1;
+            this.outcome.push("Ball " + this.balls + "!");
+
+            if (this.balls === 4) {
+                this.advanceRunners();
+
+                this.nextBatter();
+            }
+        },
+
+        foulBall() {
             this.outcome.push("Foul Ball!");
-            
+                
             if (this.strikes < 2) {
                 this.strikes += 1;
-            };
-            
-        } else if (number > 25 && number <= 30) {
-            this.outcome.push("Ground Out!");
-            
-            this.outs += 1;
-
-            if (this.outs === 3) {
-                this.nextInning();
             }
-        } else if (number > 30 && number <= 35) {
-            this.outcome.push("Fly Out!");
-            
-            this.outs += 1;
+        },
 
-            if (this.outs === 3) {
-                this.nextInning();
-            }
-        }
-        // single
-        else if (number > 50 && number <= 70) {
-          
+        single() {
             this.outcome.push("Single!")
+            
+            if (this.thirdBase) {
+                this.addRun();
+                this.thirdBase = false;
+            }
 
-          // double
-        } else if (number > 70 && number <= 85) {
-            this.outcome.push("Double!")
+            if (this.secondBase) {
+                this.thirdBase = true;
+                this.secondBase = false;
+            }
 
-          // triple
-        } else if (number > 85 && number <= 95) {
+            if (this.firstBase) {
+                this.secondBase = true;
+                this.firstBase = false;
+            }
+            
+            this.firstBase = true;
+            this.nextBatter();
+        },
+
+        double() {
+            this.outcome.push("Double!");
+
+            if (this.thirdBase) {
+                this.thirdBase = false;
+                this.addRun();
+            }
+
+            if (this.secondBase) {
+                this.secondBase = false;
+                this.addRun();
+            }
+
+            if (this.firstBase) {
+                this.firstBase = false;
+                this.thirdBase = true;
+            }
+            
+            this.secondBase = true;
+            this.nextBatter();
+        },
+
+        triple() {
             this.outcome.push("Triple!")
 
-          // home run
-        } else if (number > 95 && number <= 100) {
-            this.outcome.push("Home Run!")
-        }
-      },
-      nextInning() {
-        this.inning += 1;
-        this.balls = 0;
-        this.strikes = 0;
-        this.outs = 0;
-        this.firstBase = false;
-        this.secondBase = false;
-        this.thirdBase = false;
+            //advance runners
+            if (this.firstBase) {
+                this.firstBase = false;
+                this.addRun();
+            }
 
-      }
+            if (this.secondBase) {
+                this.secondBase = false;
+                this.addRun();
+            }
+
+            if (this.thirdBase) {
+                this.thirdBase = false;
+                this.addRun();
+            }
+
+            this.thirdBase = true;
+            this.nextBatter();
+        },
+
+        homeRun() {
+            let audio = new Audio(require('../assets/Ball+Hit+Cheer.mp3'));
+            
+            audio.play();
+            
+            this.outcome.push("Home Run!")
+
+            //advance runners
+            if (this.firstBase) {
+                this.firstBase = false;
+                this.addRun();
+            }
+
+            if (this.secondBase) {
+                this.secondBase = false;
+                this.addRun();
+            }
+
+            if (this.thirdBase) {
+                this.thirdBase = false;
+                this.addRun();
+            }
+            
+            this.addRun();
+            
+            this.nextBatter();
+
+        },
+
+        nextInning() {
+            this.inning += 1;
+            this.inningScore = 0;
+            this.balls = 0;
+            this.strikes = 0;
+            this.outs = 0;
+            this.firstBase = false;
+            this.secondBase = false;
+            this.thirdBase = false;
+        },
+        nextBatter() {
+            this.balls = 0;
+            this.strikes = 0;
+        },
+        addRun() {
+            this.inningScore += 1;
+            this.score[this.inning] = this.inningScore;
+        },
+        advanceRunners() {
+            if (this.firstBase) {
+                if(this.secondBase) {
+                    if(this.thirdBase) {
+                        // bases loaded
+                        this.firstBase = true;
+                        this.secondBase = true;
+                        this.thirdBase = true;
+
+                        this.addRun();
+                    }
+                    // first and second
+                    this.firstBase = true;
+                    this.secondBase = true;
+                    this.thirdBase = true;
+                }
+
+                // runner on first
+                this.firstBase = true;
+                this.secondBase = true;
+            }
+
+            this.firstBase = true;
+        }
+
 }}
 </script>
 
@@ -199,8 +354,12 @@ export default {
   /* border: .5px solid black; */
   border-radius: 10px;
   margin: 1rem;
-  text-decoration: none;
-  
+  text-decoration: none; 
+}
+
+.button:hover {
+    cursor: hand;
+    background-color: blue;
 }
 
 button:focus { 
